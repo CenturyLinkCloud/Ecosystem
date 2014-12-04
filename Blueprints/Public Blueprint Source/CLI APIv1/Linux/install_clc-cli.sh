bc784e7f3f0bd9a74d98d2e30897287b0a56da12
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-04 17:06:17 -0600

    -

---
+++ Blueprints/Public Blueprint Source/CLI APIv1/Linux/install_clc-cli.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# This script installs Python, pip, and the clc-cli tool.
+# After install this script creates a base configuration file to support
+# immediate cli use with provided credentials.
+#
+
+
+#########################################
+#### RHEL and RPM compatible systems
+##
+if [ -f /etc/redhat-release ]; then
+	yum -y install python-pip
+
+
+#########################################
+#### Debian and APT compatible systems
+##
+elif [ -f /etc/debian_version ]; then
+	apt-get -y update
+	apt-get -y install python-pip
+fi
+
+
+#########################################
+#### Install and configure clc-cli
+##
+pip install --upgrade clc-cli
+
+
+mkdir -p /usr/local/etc/
+cat >/usr/local/etc/clc_config <<HERE
+;
+; This configuration automatically created by the CenturyLink Cloud cli installer
+;
+
+[global]
+V1_API_KEY=$1
+V1_API_PASSWD=$2
+
+;V2_API_USERNAME=
+;V2_API_PASSWD=
+
+;blueprint_ftp_url=ftp://user:password@ftp_fqdn
+
+HERE
+
+
