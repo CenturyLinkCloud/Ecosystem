6882f95863495940556525b071b192b9c3b9450f
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-09 13:46:02 -0600

    -

--- Blueprints/Blueprint Broker/src/bp-broker/api.py
+++ Blueprints/Blueprint Broker/src/bp-broker/api.py
@@ -8,8 +8,16 @@ Listens and responds to ssl connections.  Proxies connection requests to registe
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 
+import bpbroker
 
 
 #####################################################
 
+
+web_server = BaseHTTPServer.HTTPServer(('localhost', 20443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+web_server.socket = ssl.wrap_socket (web_server.socket, 
+									 server_side=True,
+									 certfile="bp-broker/dummy_api.crt",
+									 keyfile="bp-broker/dummy_api.key")
+web_server.serve_forever()
 
