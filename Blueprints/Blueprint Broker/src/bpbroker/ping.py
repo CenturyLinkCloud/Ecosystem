161f584f69e8d2be8ba08b59d7d4a4104771ef08
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-10 11:23:12 -0600

    -

---
+++ Blueprints/Blueprint Broker/src/bpbroker/ping.py
@@ -0,0 +1,23 @@
+"""
+bp_broker ping module.
+
+Basic package to test end to end communication
+"""
+
+
+import json
+
+import bpbroker
+
+
+#####################################################
+
+def Ping(var):
+	ro = bpbroker.api.Response()
+	ro.status = 200
+	ro.response = json.dumps({'pong': var})
+
+	print ro
+	return(ro)
+
+
