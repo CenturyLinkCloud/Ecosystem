f507dbb8e7f5607a14e73179c5199ab090d9ae3d
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-22 16:00:59 -0600

    -

--- Blueprints/Blueprint Broker/src/setup.py
+++ Blueprints/Blueprint Broker/src/setup.py
@@ -9,10 +9,10 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "bpbroker",
-	version = "0.1",
+	version = "0.2",
 	packages = find_packages("."),
 
-	install_requires = ['argparse','requests','premailer'],
+	install_requires = ['argparse','requests'],
 
 	entry_points = {
 		'console_scripts': [
