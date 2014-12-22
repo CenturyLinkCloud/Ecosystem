26f04e78c6febf1c5d27f9a45c30f9073a7ce1a3
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-22 14:27:29 -0600

    -

--- Blueprints/Blueprint Broker/src/bpbroker/install.py
+++ Blueprints/Blueprint Broker/src/bpbroker/install.py
@@ -14,8 +14,106 @@ import bpbroker
 
 #####################################################
 
+INIT_D_HEADER_RPM = """#!/bin/bash
+# bpbroker daemon
+# chkconfig: 345 25 85
+# description: bpbroker service
+# processname: bpbroker
+"""
+
+INIT_D_HEADER_DEB = """#!/bin/bash
+# Provides:          bpbroker
+# Required-Start:    
+# Required-Stop:     
+# Default-Start:     3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: bpbroker service
+# Description:       BP Broker service
+"""
+
+INIT_D_SCRIPT = """
+DAEMON_PATH="/usr/local/sbin/"
+
+DAEMON=bpbroker
+DAEMONOPTS="start"
+
+NAME=bpbroker
+DESC="Blueprint Broker Service"
+PIDFILE=/var/run/$NAME.pid
+SCRIPTNAME=/etc/init.d/$NAME
+
+case "$1" in
+start)
+	printf "%-50s" "Starting $NAME..."
+	cd $DAEMON_PATH
+	PID=`$DAEMON $DAEMONOPTS > /dev/null 2>&1 & echo $!`
+	#echo "Saving PID" $PID " to " $PIDFILE
+        if [ -z $PID ]; then
+            printf "%s\n" "Fail"
+        else
+            echo $PID > $PIDFILE
+            printf "%s\n" "Ok"
+        fi
+;;
+status)
+        printf "%-50s" "Checking $NAME..."
+        if [ -f $PIDFILE ]; then
+            PID=`cat $PIDFILE`
+            if [ -z "`ps axf | grep ${PID} | grep -v grep`" ]; then
+                printf "%s\n" "Process dead but pidfile exists"
+            else
+                echo "Running"
+            fi
+        else
+            printf "%s\n" "Service not running"
+        fi
+;;
+stop)
+        printf "%-50s" "Stopping $NAME"
+            PID=`cat $PIDFILE`
+            cd $DAEMON_PATH
+        if [ -f $PIDFILE ]; then
+            kill -HUP $PID
+            printf "%s\n" "Ok"
+            rm -f $PIDFILE
+        else
+            printf "%s\n" "pidfile not found"
+        fi
+;;
+
+restart)
+  	$0 stop
+  	$0 start
+;;
+
+*)
+        echo "Usage: $0 {status|start|stop|restart}"
+        exit 1
+esac
+"""
+
+#####################################################
+
 def _InstallLinux():
-	pass
+	global INIT_D_SCRIPT
+
+	with open("/etc/init.d/bpbroker","w") as f:
+		f.write(INIT_D_SCRIPT)
+	os.system("chmod oug+x /etc/init.d/bpbroker")
+	
+	if not os.path.exists("/usr/local/sbin"):  os.makedirs("/usr/local/sbin")
+	if not os.path.exists("/usr/local/etc"):  os.makedirs("/usr/local/etc")
+
+	error = False
+	if os.path.exists("/sbin/chkconfig"):
+		# RHEL or RPM based
+		print "RHEL"
+		error = os.system("/sbin/chkconfig --add bpbroker && /sbin/chkconfig bpbroker on && /sbin/service bpbroker start")
+	elif os.path.exists("/usr/sbin/update-rc.d"):
+		print "Deb"
+		error = os.system("/usr/sbin/update-rc.d bpbroker enable && /usr/sbin/service bpbroker start")
+	else:
+		raise(Exception("Unable to install service, not RPM or DEB system"))
 
 
 def _InstallWindows():
