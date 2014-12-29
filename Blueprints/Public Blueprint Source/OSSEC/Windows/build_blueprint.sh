fc115c85cdbec58bacd3898873233cd90da2f736
Keith Resar <keith.resar@centurylinkcloud.com>
2014-12-29 12:16:29 -0600

    -

--- Blueprints/Public Blueprint Source/OSSEC/Windows/build_blueprint.sh
+++ Blueprints/Public Blueprint Source/OSSEC/Windows/build_blueprint.sh
@@ -2,15 +2,17 @@
 
 
 # Import external pre-reqs
-cp "../../../BP Broker/Windows/install_bpbroker.ps1" .
-cp "../../../BP Broker/Windows/nssm.exe" .
-cp "../../../BP Broker/Windows/python-2.7.9.zip" .
+cp "../../BP Broker/Windows/install_bpbroker.ps1" .
+cp "../../BP Broker/Windows/nssm.exe" .
+cp "../../BP Broker/Windows/python-2.7.9.zip" .
 
 
 # Create package
-zip ../../Blueprints_Completed_Packages/Windows_ossec_agent.zip  \
+zip ../Blueprints_Completed_Packages/Windows_ossec_agent.zip  \
 	install_ossec_agent.ps1 \
 	package.manifest \
+	ossec-agent-win32-2.7.1.exe \
+	ossec.conf \
 	nssm.exe \
 	python-2.7.9.zip \
 	install_bpbroker.ps1
