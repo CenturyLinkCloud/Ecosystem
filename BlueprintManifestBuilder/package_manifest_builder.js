230faac2eab721f690e755a337cfab3b242dd718
Keith Resar <keith.resar@centurylinkcloud.com>
2015-01-30 15:59:46 -0600

    -

--- BlueprintManifestBuilder/package_manifest_builder.js
+++ BlueprintManifestBuilder/package_manifest_builder.js
@@ -126,15 +126,6 @@ uuid_el.find(".el_details").remove();
 uuid_el.find(".el_help").removeClass("col-md-offset-1").removeClass("col-md-5").addClass("col-md-11").html("Each package must have a globally unique 32 character GUID.  A new value has been generated for you.  If this is replacing an existing package place update with your current GUID.  Blueprints connect to Packages using this GUID so to prevent breaking for minior changes reuse the GUID.  Likewise to create a new incompatible package release choose a new GUID.");
 uuid_el.appendTo("#built_els").show();
 
-name_el = $("#builder_el_tpl").clone().removeAttr("id");
-name_el.find(".el_btn_menu").html("");
-name_el.find(".name_span").html("Name").show();
-name_el.find("input.name").removeClass("col-md-2").addClass("col-md-10").attr("placeholder","Package Name");
-name_el.find("input.example_field").remove();
-name_el.find(".el_details").remove();
-name_el.find(".el_help").removeClass("col-md-offset-1").removeClass("col-md-5").addClass("col-md-11").html("Package name.  This is searchable and displayed when choosing the package.  CenturyLink has defined a <a href='https://t3n.zendesk.com/entries/56566304-Creating-Public-Blueprint-Packages'>naming standard for public packages</a> which you may leverage.");
-name_el.appendTo("#built_els");
-
 description_el = $("#builder_el_tpl").clone().removeAttr("id");
 description_el.find(".el_btn_menu").html("");
 description_el.find(".name_span").removeClass("col-md-2").addClass("col-md-12").html("Package Description").show();
