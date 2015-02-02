395b69c9c5eab468dccafa5e861d573f05841ade
Keith Resar <keith.resar@centurylinkcloud.com>
2015-02-02 16:42:19 -0600

    -

--- BlueprintManifestBuilder/package_manifest_builder.js
+++ BlueprintManifestBuilder/package_manifest_builder.js
@@ -1,12 +1,6 @@
 
 
-/* Generate guuid
-
-   'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-       var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-       return v.toString(16);
-   });
-*/
+$("#welcome_modal").modal('show');
 
 /******* Setup parameters ******/
 $("#chooser_container .parameter").draggable({
