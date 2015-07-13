a25eaa4ec04a05582d4b705a5962087caf6ab0e3
Keith Resar <keith.resar@centurylinkcloud.com>
2015-07-13 16:42:17 -0500

    update for ubuntu 14

--- Blueprints/Public Blueprint Source/BP Broker/Linux/install_bpbroker.sh
+++ Blueprints/Public Blueprint Source/BP Broker/Linux/install_bpbroker.sh
@@ -10,7 +10,7 @@
 command -v python >/dev/null 2>&1 || { echo "Cannot locate python, required to pre-requisite" >&2; exit 1; }
 
 yum -y install gcc python-devel libxml2-devel libxslt-devel || \
-        ( apt-get update ; apt-get -y install zlib1g-dev gcc libxml2-dev libxslt1-dev python-dev python-dev libxslt1-dev python-lxml )
+        ( apt-get -y update ; apt-get -y install zlib1g-dev gcc libxml2-dev libxslt1-dev python-dev python-dev libxslt1-dev python-lxml )
 
 # pip pre-req (Python package manager)
 command -v pip >/dev/null 2>&1 || {
@@ -22,7 +22,8 @@ command -v pip >/dev/null 2>&1 || {
 
 # Virtualenv to fence this from the system libraries
 echo "Installing and creating virtualenv"
-pip --no-cache install virtualenv
+#pip --no-cache install virtualenv
+pip install virtualenv
 virtualenv /usr/local/bpbroker
 mkdir -p /usr/local/bpbroker/{etc,lib,bin}
 source /usr/local/bpbroker/bin/activate
