17f71c296b2f133be1d0b0d129c9f778fdccf25b
Keith Resar <keith.resar@centurylinkcloud.com>
2015-09-02 13:56:47 -0500

    -

--- Blueprints/Public Blueprint Source/NAT Public IP to Primary Private IP/Linux/build_blueprint.sh
+++ Blueprints/Public Blueprint Source/NAT Public IP to Primary Private IP/Linux/build_blueprint.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Import external pre-reqs
-cp "../../../BP Broker/Linux/install_bpbroker.sh" .
 cp "../../../BP Broker/Linux/get-pip.py" .
+cp "../noarch/*.py"
+
+
+# Update version counter
+cp install.sh install.sh.$$
+if [ ! -f .build_version ]; then
+    echo "0" > .build_version
+	    git add .build_version
+		fi
+		release=$[$(cat .build_version) + 1]
+		echo $release > .build_version
+		perl -p -i -e "s/<VERSION>/$release (build `date`)/g" install.ps1
+		echo "Building version $release"
 
 
 # Create package
 zip ../../Blueprints_Completed_Packages/Linux_nat_primary_ip.zip  \
-	install_ossec_agent.sh \
 	package.manifest \
-	preloaded-vars.conf \
-	install_bpbroker.sh \
-	get-pip.py 
+	install* \
+	get-pip.py \
+	*.py
 
 
 # Cleanup
-rm install_bpbroker.sh get-pip.py
+rm install_bpbroker.sh get-pip.py *.py
 
 
