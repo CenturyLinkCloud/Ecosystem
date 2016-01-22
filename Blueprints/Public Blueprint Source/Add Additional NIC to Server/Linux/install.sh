06abeb22cac580675b72e5fec2aad8e06803b042
Keith Resar <keith.resar@centurylinkcloud.com>
2016-01-22 10:03:27 -0600

    -

--- Blueprints/Public Blueprint Source/Add Additional NIC to Server/Linux/install.sh
+++ Blueprints/Public Blueprint Source/Add Additional NIC to Server/Linux/install.sh
@@ -3,10 +3,10 @@
 CONTROL_ALIAS="${1}"
 CONTROL_USER="${2}"
 CONTROL_PASSWD="${3}"
-PORTS="${4}"
+NETWORK="${4}"
 
 
-echo "$0 version <VERSION>"
+echo "$0 version <version>"
 
 #
 # Create python sandbox
@@ -19,11 +19,10 @@ echo "$0 version <VERSION>"
 #
 mkdir -p /sysadmin
 source ./clc_api/bin/activate
-CONTROL_ALIAS="${CONTROL_ALIAS}" CONTROL_USER="${CONTROL_USER}" CONTROL_PASSWD="${CONTROL_PASSWD}" ./clc_api_nat_ip.py ${PORTS}
+CONTROL_ALIAS="${CONTROL_ALIAS}" CONTROL_USER="${CONTROL_USER}" CONTROL_PASSWD="${CONTROL_PASSWD}" NETWORK='${NETWORK}' ./clc_add_nic.py
 error=$?
 if [ $error -ne 0 ]; then
-	>&2 echo "`date`    Fatel error, exiting ($error)"
+	>&2 echo "`date`    Fatal error, exiting ($error)"
 	exit $error
 fi
-echo "Public IP: `cat /sysadmin/public_ip`"
 
